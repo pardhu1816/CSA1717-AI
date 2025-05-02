3274b26f53c4869239ced1571dd28a907ddec9aa
pardhu1816 <120441471+pardhu1816@users.noreply.github.com>
2025-05-02 13:13:14 +0530

    Add files via upload

---
+++ 4AI.py
@@ -0,0 +1,8 @@
+Python 3.10.11 (tags/v3.10.11:7d4cc5a, Apr  5 2023, 00:38:17) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/pardh/Downloads/Downloadpcgames88.com_Total_Overdose/Total Overdose PC Game/4AI.py
+SEND=9567, MORE=1085, MONEY=10652
+>>> 
+= RESTART: C:/Users/pardh/Downloads/Downloadpcgames88.com_Total_Overdose/Total Overdose PC Game/4AI.py
+SEND=9567, MORE=1085, MONEY=10652
